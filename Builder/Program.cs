d5b9d4e1bc98bc763ab2a949d5a0844548b088c1
Kuba <jciesmail@gmail.com>
2022-05-02 15:38:00 +0200

    builder

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Builder;
+
 Console.WriteLine("Hello, World!");
+
+var builder = new InvoiceBuilder();
+
+var invoice = builder.SetDate(new DateTime(2020, 1,1))
+            .SetInvoiceNumber("A1")
+            .SetVendor("Google...")
+            .SetVendee("Vendee")
+            .SetLineItems(new List<string>() { "Line item1", "Line item2" })
+            .Build();
+
+Console.WriteLine(invoice.Vendee);
+Console.WriteLine(invoice.Vendor);
